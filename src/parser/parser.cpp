4fff335e52cb4b403ff43a9b26d8066c9ad8c221
Wiktor <wiko700@gmail.com>
2024-12-12 23:39:00 +0100

    feat: add possibility to invoke SELECT query with INNER JOIN clauses

--- src/parser/parser.cpp
+++ src/parser/parser.cpp
@@ -41,8 +41,8 @@ auto Parser::process_select_query(const std::vector<std::string> &query_elements
 
     if (!is_database_selected()) return;
 
-    const auto obligatory_from_clause = std::string("FROM");
-    const auto from_clause_index = find_index(query_elements, obligatory_from_clause);
+    const auto from_clause_index = find_index(query_elements, "FROM");
+    const auto inner_clause_index = find_index(query_elements, "INNER");
 
     if (from_clause_index == -1) {
         fmt::println("Query with SELECT clause should contain FROM clause!");
@@ -50,7 +50,14 @@ auto Parser::process_select_query(const std::vector<std::string> &query_elements
     }
 
     const auto column_names = std::vector(query_elements.begin() + 1, query_elements.begin() + from_clause_index);
-    const auto table_names = std::vector(query_elements.begin() + from_clause_index + 1, query_elements.end());
+
+    if (inner_clause_index != -1) {
+        process_select_inner_join_query(query_elements, column_names);
+        return;
+    }
+
+
+    auto table_names = std::vector(query_elements.begin() + from_clause_index + 1, query_elements.end());
 
     if (column_names.size() == 1 and column_names.at(0) == "*") {
         for (const auto& table_name : table_names) {
@@ -72,14 +79,181 @@ auto Parser::process_select_query(const std::vector<std::string> &query_elements
     }
 }
 
+auto Parser::process_select_inner_join_query(
+    const std::vector<std::string>& query_elements,
+    const std::vector<std::string>& column_names
+) const -> void {
+
+    const auto from_clause_index = find_index(query_elements, "FROM");
+    const auto inner_clause_index = find_index(query_elements, "INNER");
+    const auto join_clause_index = find_index(query_elements, "JOIN");
+    const auto on_clause_index = find_index(query_elements, "ON");
+
+    if (!is_valid_select_inner_join_query(inner_clause_index, join_clause_index, on_clause_index)) {
+        fmt::println("Query with SELECT and INNER clause should contain JOIN and ON clauses!");
+        return;
+    }
+
+    auto table_names = std::vector(query_elements.begin() + from_clause_index + 1, query_elements.begin() +  inner_clause_index);
+    table_names.emplace_back(query_elements.at(join_clause_index + 1));
+
+    const auto& left = query_elements.at(on_clause_index + 1);
+    const auto& right = query_elements.at(on_clause_index + 3);
+
+    if (!validate_tables_and_columns(left, right, query_elements)) return;
+
+    if (column_names.size() == 1 and column_names.at(0) == "*") {
+        print_all_columns_for_inner_join(left, right);
+        return;
+    }
+
+    print_specific_columns_for_inner_join(left, right, column_names);
+}
+
+auto Parser::is_valid_select_inner_join_query(
+    const int inner_clause_index,
+    const int join_clause_index,
+    const int on_clause_index
+) const -> bool {
+    return join_clause_index == inner_clause_index + 1 && on_clause_index == join_clause_index + 2;
+}
+
+auto Parser::validate_tables_and_columns(
+    const std::string& left,
+    const std::string& right,
+    const std::vector<std::string>& query_elements
+) const -> bool {
+
+    const auto from_clause_index = find_index(query_elements, "FROM");
+    const auto join_clause_index = find_index(query_elements, "JOIN");
+
+    const auto [left_table_name, left_column_name] = split_string_with_dot(left);
+    const auto [right_table_name, right_column_name] = split_string_with_dot(right);
+
+    if (!database.value().tables.contains(left_table_name)) {
+        fmt::println("Table with name '{}' not exists in database '{}'!", left_table_name, database.value().name);
+        return false;
+    }
+
+    if (!database.value().tables.contains(right_table_name)) {
+        fmt::println("Table with name '{}' not exists in database '{}'!", right_table_name, database.value().name);
+    }
+
+    if (left_table_name != query_elements.at(from_clause_index + 1)) {
+        fmt::println("Left table name after ON clause not match table name after FROM clause!");
+        return false;
+    }
+
+    if (right_table_name != query_elements.at(join_clause_index + 1)) {
+        fmt::println("Right table name after ON clause not match table name after JOIN clause!");
+        return false;
+    }
+
+    auto left_table = database.value().tables.find(left_table_name)->second;
+    auto right_table = database.value().tables.find(right_table_name)->second;
+
+    if (std::ranges::find(left_table.column_names, left_column_name) == left_table.column_names.end()) {
+        fmt::println("'{}' column not exists in table '{}'!", left_column_name, left_table_name);
+        return false;
+    }
+
+    if (std::ranges::find(right_table.column_names, right_column_name) == right_table.column_names.end()) {
+        fmt::println("'{}' column not exists in table '{}'!", right_column_name, right_table_name);
+        return false;
+    }
+
+    return true;
+}
+
+auto Parser::print_all_columns_for_inner_join(
+    const std::string& left,
+    const std::string& right
+) const -> void {
+    auto data = std::vector<std::vector<std::string>>{};
+
+    const auto [left_table_name, left_column_name] = split_string_with_dot(left);
+    const auto [right_table_name, right_column_name] = split_string_with_dot(right);
+
+    auto left_table = database.value().tables.find(left_table_name)->second;
+    auto right_table = database.value().tables.find(right_table_name)->second;
+
+    const auto left_column_id = Table::find_index(left_table.column_names, left_column_name);
+    const auto right_column_id = Table::find_index(right_table.column_names, right_column_name);
+
+    for (auto left_table_data : left_table.get_data()) {
+
+        auto combined_data = std::vector<std::string>{};
+
+        for (auto right_table_data : right_table.get_data()) {
+
+            if (left_table_data[left_column_id] == right_table_data[right_column_id]) {
+                for (const auto& element : left_table_data) combined_data.push_back(element);
+                for (const auto& element : right_table_data) combined_data.push_back(element);
+                data.push_back(combined_data);
+                combined_data.clear();
+            }
+        }
+    }
+
+    fmt::println("{}", data);
+}
+
+auto Parser::print_specific_columns_for_inner_join(
+    const std::string& left,
+    const std::string& right,
+    const std::vector<std::string>& column_names
+) const -> void {
+    auto data = std::vector<std::vector<std::string>>{};
+
+    const auto [left_table_name, left_column_name] = split_string_with_dot(left);
+    const auto [right_table_name, right_column_name] = split_string_with_dot(right);
+
+    auto left_table = database.value().tables.find(left_table_name)->second;
+    auto right_table = database.value().tables.find(right_table_name)->second;
+
+    const auto left_column_id = Table::find_index(left_table.column_names, left_column_name);
+    const auto right_column_id = Table::find_index(right_table.column_names, right_column_name);
+
+    for (const auto& left_table_data : left_table.get_data()) {
+        for (const auto& right_table_data : right_table.get_data()) {
+            if (left_table_data[left_column_id] == right_table_data[right_column_id]) {
+                auto combined_data = std::vector<std::string>{};
+                for (auto column_name : column_names) {
+                    std::erase(column_name, ',');
+                    if (const auto dot_pos = column_name.find('.'); dot_pos != std::string::npos) {
+                        const auto [t_name, c_name] = split_string_with_dot(column_name);
+                        if (t_name == left_table_name) combined_data.push_back(left_table_data[Table::find_index(left_table.column_names, c_name)]);
+                        else if (t_name == right_table_name) combined_data.push_back(right_table_data[Table::find_index(right_table.column_names, c_name)]);
+                    } else {
+                        const auto c_left_index = Table::find_index(left_table.column_names, column_name);
+                        const auto c_right_index = Table::find_index(right_table.column_names, column_name);
+
+                        if (c_left_index != -1 && c_right_index != -1) {
+                            fmt::println("Column with name '{}' exists in both tables!", column_name);
+                            return;
+                        }
+
+                        if (c_left_index != -1) for (const auto& element : left_table_data) combined_data.push_back(element);
+                        if (c_right_index != -1) for (const auto& element : right_table_data) combined_data.push_back(element);
+                    }
+                }
+                data.push_back(combined_data);
+            }
+        }
+    }
+
+    fmt::println("{}", data);
+}
+
+
+
 auto Parser::process_insert_query(const std::vector<std::string> &query_elements) -> void {
 
     if (!is_database_selected()) return;
 
     if (query_elements.at(1) == "INTO") {
 
-        const auto obligatory_values_clause = std::string("VALUES");
-        const auto values_clause_index = find_index(query_elements, obligatory_values_clause);
+        const auto values_clause_index = find_index(query_elements, "VALUES");
 
         if (values_clause_index == -1) {
             fmt::println("Query with INSERT clause should contain VALUES clause!");
@@ -102,8 +276,7 @@ auto Parser::process_alter_query(const std::vector<std::string>& query_elements)
 
     if (query_elements.at(1) == "TABLE") {
 
-        const auto obligatory_column_clause = std::string("COLUMN");
-        const auto column_clause_index = find_index(query_elements, obligatory_column_clause);
+        const auto column_clause_index = find_index(query_elements, "COLUMN");
         const auto operation_clause_index = column_clause_index - 1;
         const auto& table_name = query_elements.at(2);
 
@@ -159,9 +332,7 @@ auto Parser::process_table_query(const std::vector<std::string> &query_elements)
 
                     if (!column_foreign_keys_string.empty()) {
 
-                        const auto dot_pos = column_foreign_keys_string.find('.');
-                        auto foreign_table_name = column_foreign_keys_string.substr(0, dot_pos);
-                        auto foreign_column_name = column_foreign_keys_string.substr(dot_pos + 1);
+                        const auto [foreign_table_name, foreign_column_name] = split_string_with_dot(column_foreign_keys_string);
 
                         if (!is_foreign_key_valid(foreign_table_name, foreign_column_name, column_type)) return;
 
@@ -274,6 +445,10 @@ auto Parser::is_foreign_key_valid(
     return true;
 }
 
+auto Parser::split_string_with_dot(const std::string& str) const -> std::pair<std::string, std::string> {
+    const auto dot_pos = str.find('.');
+    auto left = str.substr(0, dot_pos);
+    auto right = str.substr(dot_pos + 1);
 
-
-
+    return std::pair{left, right};
+}
--- src/parser/parser.h
+++ src/parser/parser.h
@@ -20,6 +20,29 @@ public:
 private:
     auto process_database_query(const std::vector<std::string>& query_elements) -> void;
     auto process_select_query(const std::vector<std::string>& query_elements) const -> void;
+    auto process_select_inner_join_query(
+        const std::vector<std::string>& query_elements,
+        const std::vector<std::string>& column_names
+    ) const -> void;
+    auto is_valid_select_inner_join_query(
+        int inner_clause_index,
+        int join_clause_index,
+        int on_clause_index
+    ) const -> bool;
+    auto validate_tables_and_columns(
+        const std::string& left,
+        const std::string& right,
+        const std::vector<std::string>& query_elements
+    ) const -> bool;
+    auto print_all_columns_for_inner_join(
+    const std::string& left,
+    const std::string& right
+    ) const -> void;
+    auto print_specific_columns_for_inner_join(
+        const std::string& left,
+        const std::string& right,
+        const std::vector<std::string>& column_names
+    ) const -> void;
     auto process_insert_query(const std::vector<std::string>& query_elements) -> void;
     auto process_alter_query(const std::vector<std::string>& query_elements) -> void;
     auto process_table_query(const std::vector<std::string>& query_elements) -> void;
@@ -31,5 +54,6 @@ private:
         const std::string& foreign_column_name,
         const std::string& column_type
     ) const -> bool;
+    auto split_string_with_dot(const std::string& str) const -> std::pair<std::string, std::string>;
 };
 
