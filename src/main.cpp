a802fcecac6530f26dcb727930414b5142bc0200
Wiktor <wiko700@gmail.com>
2025-01-18 15:46:53 +0100

    feat: add saving databases data always before closing program

--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,31 @@
 #include "database/database.h"
 #include "parser/parser.h"
 #include "serializer/serializer.h"
+#include <csignal>
+
+auto handle_signal(const int signal) -> void{
+    Serializer::save_databases_to_file();
+    std::exit(signal);
+}
 
 int main() {
-
     Serializer::upload_databases_from_file();
+
+    std::signal(SIGINT, handle_signal);
+    std::signal(SIGTERM, handle_signal);
+
     auto parser = Parser();
 
-    //users.id, users.name, pets.name, height, weight
     auto query = std::string();
-    while (true) {
+    while (query != "EXIT") {
         fmt::print("Query: ");
         std::getline(std::cin, query);
 
-        if (query.empty()) break;
+        if (query == "EXIT") break;
+        if (query.empty()) {
+            fmt::println("Can't process empty query!");
+            continue;
+        }
 
         parser.parse_query(query);
     }
--- src/parser/parser.h
+++ src/parser/parser.h
@@ -5,6 +5,7 @@
 #include <optional>
 
 #include "../database/database.h"
+#include "../serializer/serializer.h"
 
 class Parser {
 
@@ -14,6 +15,10 @@ public:
     Parser() = default;
 
     explicit Parser(Database* database) : database(database) {}
+
+    ~Parser() {
+        Serializer::save_databases_to_file();
+    }
 
     auto parse_query(const std::string& query) -> void;
     [[nodiscard]] auto is_database_selected() const -> bool;
